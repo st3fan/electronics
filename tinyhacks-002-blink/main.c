db3d52f679767c72e632317eb4d342cd38db2507
Stefan Arentz <stefan@arentz.ca>
2010-07-21 11:40:29 -0400

    Fixed the code to use the built-in _delay_ms() function. Which works correctly with any CPU speed

--- tinyhacks-002-blink/main.c
+++ tinyhacks-002-blink/main.c
@@ -1,35 +1,25 @@
-// Blink2313 - Blink a LED on PD5 of a ATTiny 2313
+// blink a LED that is connected to PB4 of a Tiny45
 
 #include <avr/io.h>
-#include <avr/delay.h>
-
-void mydelay(uint8_t delay)
-{
-    int n = 0;
-    for (int i = 0; i < delay; i++) {
-        for (int j = 0; j < 255; j++) {
-            n++;
-        }
-    }
-}
+#include <util/delay.h>
 
 int main(void)
 {
-    DDRB = (1 << PB0);
+    DDRB = (1 << PB4);
 
     while (1)
     {
         for (uint8_t delay = 10; delay <= 100; delay += 10) {
             for (uint8_t i = 0; i < 4; i++) {
-                mydelay(delay);
-                PORTB ^= (1 << PB0);
+                _delay_ms(delay);
+                PORTB ^= (1 << PB4);
             }
         }
         
         for (uint8_t delay = 100; delay >= 10; delay -= 10) {
             for (uint8_t i = 0; i < 4; i++) {
-                mydelay(delay);
-                PORTB ^= (1 << PB0);
+                _delay_ms(delay);
+                PORTB ^= (1 << PB4);
             }
         }
     }
